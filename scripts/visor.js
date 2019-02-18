54fa2874a671aac9d6d866fb5d3820bcdd1ad3db
Diegos Lores <diegoslores@gmail.com>
2019-02-18 09:16:33 +0100

    primeros pasos de añadir js

---
+++ scripts/visor.js
@@ -0,0 +1,78 @@
+function mapa() {
+  let erase = `<div id="map"></div>`;
+  document.querySelector(".imgVisor").innerHTML = erase;
+
+  let map = L.map("map").setView([41.66, -4.72], 15);
+  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+  }).addTo(map);
+  L.control.scale().addTo(map);
+  L.marker([41.66, -4.71], {
+    draggable: true
+  }).addTo(map);
+}
+
+function relieve() {
+  let erase = `<div id="map"></div>`;
+  document.querySelector(".imgVisor").innerHTML = erase;
+
+  let map = L.map("map").setView([50.66, -4.72], 15);
+  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+  }).addTo(map);
+  L.control.scale().addTo(map);
+  L.marker([41.66, -4.71], {
+    draggable: true
+  }).addTo(map);
+}
+
+function satelite() {
+  let erase = `<div id="map"></div>`;
+  document.querySelector(".imgVisor").innerHTML = erase;
+
+  let map = L.map("map").setView([50.66, -4.72], 15);
+  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+  }).addTo(map);
+  L.control.scale().addTo(map);
+  L.marker([41.66, -4.71], {
+    draggable: true
+  }).addTo(map);
+}
+
+function americano() {
+  let erase = `<div id="map"></div>`;
+  document.querySelector(".imgVisor").innerHTML = erase;
+
+  let map = L.map("map").setView([50.66, -4.72], 15);
+  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+  }).addTo(map);
+  L.control.scale().addTo(map);
+  L.marker([41.66, -4.71], {
+    draggable: true
+  }).addTo(map);
+}
+function catastro() {
+  let erase = `<div id="map"></div>`;
+  document.querySelector(".imgVisor").innerHTML = erase;
+
+  let map = L.map("map").setView([50.66, -4.72], 15);
+  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+  }).addTo(map);
+  L.control.scale().addTo(map);
+  L.marker([41.66, -4.71], {
+    draggable: true
+  }).addTo(map);
+}
