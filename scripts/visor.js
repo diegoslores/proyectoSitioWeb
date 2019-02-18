e49326fc28395e22d9b95c0a60276767b1dbafd7
Diegos Lores <diegoslores@gmail.com>
2019-02-18 10:32:52 +0100

    script leaflet implementado.

--- scripts/visor.js
+++ scripts/visor.js
@@ -2,14 +2,14 @@ function mapa() {
   let erase = `<div id="map"></div>`;
   document.querySelector(".imgVisor").innerHTML = erase;
 
-  let map = L.map("map").setView([41.66, -4.72], 15);
+  let map = L.map("map").setView([42.88, -8.54], 12.5);
   L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
   }).addTo(map);
   L.control.scale().addTo(map);
-  L.marker([41.66, -4.71], {
+  L.marker([42.88, -8.54], {
     draggable: true
   }).addTo(map);
 }
@@ -18,14 +18,24 @@ function relieve() {
   let erase = `<div id="map"></div>`;
   document.querySelector(".imgVisor").innerHTML = erase;
 
-  let map = L.map("map").setView([50.66, -4.72], 15);
-  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-  }).addTo(map);
+  let map = L.map("map").setView([42.88, -8.54], 12.5);
+
+  L.tileLayer
+    .wms("http://www.ign.es/wms-inspire/mapa-raster", {
+      layers: "mtn_rasterizado",
+      format: "image/png",
+      transparent: false,
+      continuousWorld: true,
+      minZoom: 8,
+      maxZoom: 18,
+      attribution:
+        'Â© <a href="http://www.ign.es/ign/main/index.do" target="_blank">Instituto GeogrÃ¡fico Nacional de EspaÃ±a</a>'
+    })
+    .addTo(map);
+
   L.control.scale().addTo(map);
-  L.marker([41.66, -4.71], {
+
+  L.marker([42.88, -8.54], {
     draggable: true
   }).addTo(map);
 }
@@ -34,14 +44,24 @@ function satelite() {
   let erase = `<div id="map"></div>`;
   document.querySelector(".imgVisor").innerHTML = erase;
 
-  let map = L.map("map").setView([50.66, -4.72], 15);
-  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-  }).addTo(map);
+  let map = L.map("map").setView([42.88, -8.54], 12.5);
+
+  L.tileLayer
+    .wms("http://www.ign.es/wms-inspire/pnoa-ma", {
+      layers: "OI.OrthoimageCoverage",
+      format: "image/png",
+      transparent: true,
+      minZoom: 8,
+      maxZoom: 18,
+      continuousWorld: true,
+      attribution:
+        'PNOA cedido por &copy; <a href="http://www.ign.es/ign/main/index.do" target="_blank">Instituto GeogrÃ¡fico Nacional de EspaÃ±a</a>'
+    })
+    .addTo(map);
+
   L.control.scale().addTo(map);
-  L.marker([41.66, -4.71], {
+
+  L.marker([42.88, -8.54], {
     draggable: true
   }).addTo(map);
 }
@@ -50,29 +70,45 @@ function americano() {
   let erase = `<div id="map"></div>`;
   document.querySelector(".imgVisor").innerHTML = erase;
 
-  let map = L.map("map").setView([50.66, -4.72], 15);
-  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-  }).addTo(map);
+  let map = L.map("map").setView([42.88, -8.54], 12.5);
+
+  L.tileLayer
+    .wms("http://www.ign.es/wms/pnoa-historico?", {
+      layers: "AMS_1956-1957",
+      format: "image/png",
+      transparent: false,
+      minZoom: 8,
+      maxZoom: 18,
+      attribution:
+        'Voo americano 1956-1957 cedido por &copy; <a href="http://www.ign.es/ign/main/index.do" target="_blank">Instituto GeogrÃ¡fico Nacional de EspaÃ±a</a>'
+    })
+    .addTo(map);
+
   L.control.scale().addTo(map);
-  L.marker([41.66, -4.71], {
+
+  L.marker([42.88, -8.54], {
     draggable: true
   }).addTo(map);
 }
+
 function catastro() {
   let erase = `<div id="map"></div>`;
   document.querySelector(".imgVisor").innerHTML = erase;
 
-  let map = L.map("map").setView([50.66, -4.72], 15);
-  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18
-  }).addTo(map);
+  let map = L.map("map").setView([42.88, -8.54], 14);
+
+  L.tileLayer
+    .wms("http://ovc.catastro.meh.es/Cartografia/WMS/ServidorWMS.aspx?", {
+      layers: "Catastro",
+      format: "image/jpeg",
+      transparent: true,
+      version: "1.1.1",
+      attribution: "DIRECCION GENERAL DEL CATASTRO"
+    })
+    .addTo(map);
+
   L.control.scale().addTo(map);
-  L.marker([41.66, -4.71], {
+  L.marker([42.88, -8.54], {
     draggable: true
   }).addTo(map);
 }
